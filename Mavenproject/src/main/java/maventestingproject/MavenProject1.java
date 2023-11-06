cf17b1b04e26a00c5c80ed59d42e73d015720d68
Nisha Sharma <124479532+hhggfdsds@users.noreply.github.com>
2023-11-05 21:08:37 -0800

    Maven project for testing prospect

---
+++ Mavenproject/src/main/java/maventestingproject/MavenProject1.java
@@ -0,0 +1,11 @@
+package maventestingproject;
+
+public class MavenProject1 {
+
+	public static void main(String[] args) {
+		System.out.println("1st maven project");
+		System.out.println("New offers for black friday");
+
+	}
+
+}
